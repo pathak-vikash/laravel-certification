e187e432da64fa01f0524e13fc212e5678aebcfd
Vikash Pathak <vikashh.pathak@gmail.com>
2019-12-24 12:20:16 +0530

    Updated readme

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,29 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+
+Artisan::command('demo', function () {
+    //dd($this->arguments());
+
+    $name = $this->ask('What is your name?');
+    $password = $this->secret('What is the password?');
+
+    $this->info("Name: ". $name);
+    $this->info("Password: ". $password);
+
+    # asking for confirmation
+    if ($this->confirm('Do you wish to continue?')) {
+        $this->line("You have selected Yes");
+    }else{
+        $this->error("You have selected no");
+    }
+
+    # Auto -completion
+    $name = $this->anticipate('What is your name?', ['Taylor', 'Dayle']);
+
+    # Multiple Choices
+    $name = $this->choice('What is your name?', ['Taylor', 'Dayle'], 1);
+})->describe('Display an inspiring quote');
+
+
