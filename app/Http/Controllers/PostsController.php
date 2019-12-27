507f5bb6528c81bff7c00838de90d31fcdf4784c
Vikash Pathak <vikashh.pathak@gmail.com>
2019-12-27 18:28:00 +0530

    Added virtual attribute - with appends

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,9 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::with('author')->get();
+
+        return $posts;
     }
 
     /**
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    
+    public $appends = ["full_content"];
+
+    public function author(){
+        return $this->belongsTo(\App\User::class, "author_id");
+    }
+
+
+    public function getContentAttribute($value){
+        return substr($value, 0, 100);
+    }
+
+    public function getFullContentAttribute(){
+        return $this->attributes['content'];
+    }
 }
