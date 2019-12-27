4bfe39a4d3830adf4e337f5cb2596d68ba1b8876
Vikash Pathak <vikashh.pathak@gmail.com>
2019-12-27 18:16:05 +0530

    Created factory + users seeder + relation with posts

--- app/User.php
+++ app/User.php
@@ -28,4 +28,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class, "author_id");
+    }
 }
